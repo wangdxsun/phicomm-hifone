78ee22bb764917ceea36308040b8736cd4dbd08f
qiuling.jiang <qiuling.jiang@phicomm.com>
2017-05-10 10:39:49 +0800

    增加cnzz统计

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,10 @@
         <div class="copyright">
             <blockquote class="pull-left">
                 <p>{!! Config::get('setting.site_about') !!}</p>
-                <p>{!! trans('hifone.powered_by') !!} <span class="pipe">|</span><a href="http://www.miitbeian.gov.cn/">沪ICP备09048395号-17</a></p>
+                <p>
+                    {!! trans('hifone.powered_by') !!} <span class="pipe">|</span><a href="http://www.miitbeian.gov.cn/">沪ICP备09048395号-17</a>
+                    <span>| <script src="https://s4.cnzz.com/z_stat.php?id={{ env('CNZZ_STATICS') }}&web_id={{ env('CNZZ_STATICS') }}" language="JavaScript"></script></span>
+                </p>
             </blockquote>
         </div>
         <div class="pull-right hidden-sm hidden-xs">
