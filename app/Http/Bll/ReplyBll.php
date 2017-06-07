dfec9f193458a74af27b6e066dea366e5e97836e
qiuling.jiang <qiuling.jiang@phicomm.com>
2017-06-07 19:08:15 +0800

    修复网页版发表回复未正常解析markdown

--- app/Http/Bll/ReplyBll.php
+++ app/Http/Bll/ReplyBll.php
@@ -18,9 +18,9 @@ class ReplyBll extends BaseBll
     public function createReply()
     {
         $replyData = Input::get('reply');
-        $replyData['body'] = "<p>".$replyData['body']."</p>";
         //如果有单独上传图片，将图片拼接到正文后面
         if (Input::has('images')) {
+            $replyData['body'] = "<p>".$replyData['body']."</p>";
             foreach ($images = Input::get('images') as $image) {
                 $res = dispatch(new UploadBase64ImageCommand($image));
                 $replyData['body'] .= "<img src='{$res["filename"]}'/>";
