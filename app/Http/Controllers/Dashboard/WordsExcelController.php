19574a62329b4ec94d3818ff8f5e3694a51b8091
wangdxsun <daoxin.wang@phicomm.com>
2017-07-24 14:53:08 +0800

    敏感词去重且导入数目不大于5000

--- app/Http/Controllers/Dashboard/WordsExcelController.php
+++ app/Http/Controllers/Dashboard/WordsExcelController.php
@@ -11,11 +11,13 @@ use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Redirect;
 use Hifone\Models\Word;
 use Illuminate\Support\Facades\Cache;
+use Mockery\Exception;
 
 class WordsExcelController extends  Controller
 {
     //Excel导出
-    public function export(){
+    public function export()
+    {
 
         $data = Word::get()->toArray();
         Excel::create('illegal_words',function($excel) use ($data){
@@ -29,7 +31,7 @@ class WordsExcelController extends  Controller
 
     public function import()
     {
-        if(Input::hasFile('import_file')){
+        if(Input::hasFile('import_file')) {
             $path = Input::file('import_file')->getRealPath();
             $original_name = Input::file('import_file')->getClientOriginalName();
 
@@ -37,40 +39,38 @@ class WordsExcelController extends  Controller
             $file_type = $file_types[count($file_types)-1];
 
             //判断是否.xls文件
-            if(strtolower($file_type =='xls') || strtolower($file_type =='xlsx') ){
+            if(strtolower($file_type =='xls') || strtolower($file_type =='xlsx') ) {
                 $data = Excel::load($path, function($reader) {})->all();
-                if(!empty($data) && $data->count()){
-                    $insert=[];
-                    //放入缓存
-                    $words = Word::all()->pluck('word');
-                    foreach ($words as $key => $value) {
-                        Cache::put($value, $value, 1);
-                    }
-                    foreach ($data as $key => $value) {
-                        //删除重复敏感词数据库记录
-                        if (Cache::has($value->word)) {
-                            Word::where('word',$value->word)->delete();
-                            Cache::pull($value->word);
+                if ($data->count() <= 5000) {
+                    if ($data) {
+                        $insert = [];
+                        $words = Word::all()->pluck('word')->toArray();
+                        foreach ($data as $key => $value) {
+                            //删除重复敏感词数据库记录
+                            if (array_search($value->word, $words) !== false) {
+                                Word::where('word',$value->word)->delete();
+                            }
+                            if(!empty($value->word)){
+                                $insert[] = [
+                                    'last_op_user_id' => $value->last_op_user_id,
+                                    'type' => $value->type,
+                                    'word' => $value->word,
+                                    'status' => $value->status,
+                                    'replacement' => $value->replacement,
+                                    'created_at' => date('Y-m-d H:i:s'),
+                                    'last_op_time' => date('Y-m-d H:i:s'),
+                                ];
+                            }
                         }
-//                        dd(Word::limit(5)->get()->pluck('word'));//pluck返回集合
-//                        && !array_search($value->word, $insert)
-                        if(!empty($value->word)){
-                            $insert[] = [
-                                'last_op_user_id' => $value->last_op_user_id,
-                                'type' => $value->type,
-                                'word' => $value->word,
-                                'status' => $value->status,
-                                'replacement' => $value->replacement,
-                                'created_at' => date('Y-m-d H:i:s'),
-                                'last_op_time' => date('Y-m-d H:i:s'),
-                            ];
-                        }
+                        Word::insert($insert);//批量创建
+                        return Redirect::route('dashboard.word.index')
+                            ->withSuccess(sprintf('%s %s', trans('hifone.awesome'), '导入成功'));
                     }
-                    Word::insert($insert);//批量创建
+                } else {
                     return Redirect::route('dashboard.word.index')
-                        ->withSuccess(sprintf('%s %s', trans('hifone.awesome'), '导入成功'));
-
+                        ->withSuccess(sprintf('%s %s', trans('hifone.failure'), '敏感词条目不大于5000条'));
                 }
+
             }else{
                 return Redirect::route('dashboard.word.index')
                     ->withSuccess(sprintf('%s %s', trans('hifone.failure'), '文件格式不正确'));
@@ -78,3 +78,5 @@ class WordsExcelController extends  Controller
         }
     }
 }
+
+
