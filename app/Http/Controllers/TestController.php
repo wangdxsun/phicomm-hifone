e9237d4cdc0a56c40d339977114ccda8b1e6367f
qiuling.jiang <qiuling.jiang@phicomm.com>
2017-11-03 13:50:53 +0800

    隐藏回复编辑框插入代码按钮

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: meng.dai
+ * Date: 2017/8/4
+ * Time: 9:25
+ */
+namespace Hifone\Http\Controllers;
+use Hifone\Models\Thread;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
+
+class GetFirstImgUrlController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
