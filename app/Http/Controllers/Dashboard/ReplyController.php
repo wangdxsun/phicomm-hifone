e8519353a6744f9ac0246345ef11bbd81edcf644
daimeng <meng.dai163@gmail.com>
2017-11-07 16:14:59 +0800

    先删除帖子再删除帖子下评论，不更新搜索索引

--- app/Http/Controllers/Dashboard/ReplyController.php
+++ app/Http/Controllers/Dashboard/ReplyController.php
@@ -245,7 +245,9 @@ class ReplyController extends Controller
             $reply->thread->node->decrement('reply_count', 1);//版块回帖数-1
             $reply->thread->subNode->decrement('reply_count', 1);//子版块回帖数-1
             $reply->thread->decrement('reply_count', 1);
-            $reply->thread->updateIndex();
+            if ($reply->thread->status == 0) {
+                $reply->thread->updateIndex();
+            }
             $reply->user->decrement('reply_count', 1);
             event(new ReplyWasTrashedEvent($reply));
             DB::commit();
