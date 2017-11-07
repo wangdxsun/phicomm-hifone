e5633199f6d1b5bd0347f43435e4d0c688eeddcc
qiuling.jiang <qiuling.jiang@phicomm.com>
2017-11-07 18:55:31 +0800

    消息中心增加分页

--- app/Http/Bll/NotificationBll.php
+++ app/Http/Bll/NotificationBll.php
@@ -15,9 +15,7 @@ class NotificationBll extends BaseBll
 {
     public function watch()
     {
-        Auth::user()->notification_follow_count = 0;
-        Auth::user()->save();
-        $notifications = Notification::forUser(Auth::id())->watch()->recent()->with(['object'])->get();
+        $notifications = Notification::forUser(Auth::id())->watch()->recent()->with(['object'])->paginate();
         foreach ($notifications as $key => &$notification) {
             if (!$notification->object->visible) {
                 unset($notifications[$key]);
@@ -26,13 +24,15 @@ class NotificationBll extends BaseBll
                 $notification->object->user;
             }
         }
+        Auth::user()->notification_follow_count = 0;
+        Auth::user()->save();
 
         return $notifications;
     }
 
     public function reply()
     {
-        $notifications = Notification::forUser(Auth::id())->ofType('thread_new_reply')->recent()->with(['object', 'author'])->get();
+        $notifications = Notification::forUser(Auth::id())->ofType('thread_new_reply')->recent()->with(['object', 'author'])->paginate();
         foreach ($notifications as $key => &$notification) {
             if (empty($notification->object) || !$notification->object->visible || !$notification->object->thread->visible) {//兼容回收站帖子-1状态的老数据
                 unset($notifications[$key]);
@@ -46,7 +46,7 @@ class NotificationBll extends BaseBll
 
     public function at()
     {
-        $notifications = Notification::forUser(Auth::id())->at()->recent()->with(['object', 'author'])->get();
+        $notifications = Notification::forUser(Auth::id())->at()->recent()->with(['object', 'author'])->paginate();
         foreach ($notifications as $key => &$notification) {
             if (!$notification->object->visible || ($notification->object_type == 'Hifone\Models\Reply' && !$notification->object->thread->visible)) {
                 unset($notifications[$key]);
@@ -54,12 +54,13 @@ class NotificationBll extends BaseBll
         }
         Auth::user()->notification_at_count = 0;
         Auth::user()->save();
+
         return $notifications;
     }
 
     public function system()
     {
-        $notifications = Notification::forUser(Auth::id())->system()->recent()->with(['object', 'author'])->get();
+        $notifications = Notification::forUser(Auth::id())->system()->recent()->with(['object', 'author'])->paginate();
         foreach ($notifications as $key => &$notification) {
             if ($notification->type <> 'user_follow' && !$notification->object->visible) {
                 unset($notifications[$key]);
