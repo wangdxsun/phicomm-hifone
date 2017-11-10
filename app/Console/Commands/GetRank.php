27395ce56f9fe8fff122cb428a35ff72cda129a9
daimeng <meng.dai163@gmail.com>
2017-11-10 10:11:43 +0800

    修复排行榜时间显示的错误

--- app/Console/Commands/GetRank.php
+++ app/Console/Commands/GetRank.php
@@ -23,7 +23,7 @@ class GetRank extends Command
     public function handle()
     {
         $lastMonday = Carbon::today()->previousWeekendDay()->subDay(6)->toDateTimeString();
-        $lastSunday = Carbon::today()->previousWeekendDay()->addDay(1)->toDateTimeString();
+        $lastSunday = Carbon::today()->previousWeekendDay()->subSecond()->subSecond()->toDateTimeString();
         $userRankCount = [];
         $week_rank = 0;
         //处理点赞逻辑，首先拿到上个星期被点赞了的帖子和回复，以及对应的用户信息
