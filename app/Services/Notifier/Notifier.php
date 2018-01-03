2b0914aaaaf7395e5b51083919b6fd5a7f2b0c22
Daoxin Wang/王道鑫/PD/SMART HOME <daoxin.wang@phicomm.com>
2018-01-03 11:18:41 +0800

    web端 评论回复@我增加消息推送

--- app/Services/Notifier/Notifier.php
+++ app/Services/Notifier/Notifier.php
@@ -12,6 +12,7 @@
 namespace Hifone\Services\Notifier;
 
 use Carbon\Carbon;
+use Hifone\Http\Bll\BaseBll;
 use Hifone\Models\Notification;
 use Hifone\Models\Reply;
 use Hifone\Models\Thread;
@@ -36,8 +37,14 @@ class Notifier
             $toUser->increment('notification_system_count', 1);
         } elseif ($type == 'reply_reply' || $type == 'reply_mention') {
             $toUser->increment('notification_at_count', 1);
+
+            //消息推送
+            $this->push($author, $toUser, "1002", $data['body']);
         } elseif ($type == 'thread_new_reply') {
             $toUser->increment('notification_reply_count', 1);
+
+            //消息推送
+            $this->push($author, $toUser, "1001", $data['body']);
         }
 
         $object->notifications()->create($data);
@@ -58,8 +65,14 @@ class Notifier
             ];
             if ($type == 'thread_new_reply') {
                 $toUser->increment('notification_reply_count');
+
+                //消息推送
+                $this->push($author, $toUser, "1001", $data['body']);
             } elseif ($type == 'reply_mention') {
                 $toUser->increment('notification_at_count');
+
+                //消息推送
+                $this->push($author, $toUser, "1002", $data['body']);
             } elseif ($type == 'followed_user_new_thread') {
                 $toUser->increment('notification_follow_count');
             }
@@ -90,5 +103,26 @@ class Notifier
         }
 
         return $notYetNotifyUsers;
+    }
+
+    /**
+     * @param $from
+     * @param $to
+     * @param $type thread_new_reply:1001; reply_reply, reply_mention:1002;
+     * @param $content
+     */
+    protected function push($from, $to, $type, $content)
+    {
+        $data = array(
+            'message' => $content,
+            'type' => $type,
+            'msg_type' => '0',//推送消息类型 0.通知,1.消息
+            'outline' => substr($content, 0, 26),
+            'title' => $from->username,
+            'uid' => $to->phicomm_id,
+            'url' => route('app.chat.message', ['user' => $from->id, 'scope' => 'after']),
+        );
+
+        (new BaseBll())->pushMessage($data);
     }
 }
