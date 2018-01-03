0bf06d8f8bfa045d8cb68066ddf9e580369c5df6
qiuling.jiang <qiuling.jiang@phicomm.com>
2018-01-03 09:22:54 +0800

    web端上传图片接口改成单张图片上传

--- app/Http/Controllers/Web/CommonController.php
+++ app/Http/Controllers/Web/CommonController.php
@@ -8,6 +8,9 @@
 
 namespace Hifone\Http\Controllers\Web;
 
+use Hifone\Commands\Image\UploadBase64ImageCommand;
+use Hifone\Commands\Image\UploadImageCommand;
+use Hifone\Exceptions\HifoneException;
 use Hifone\Http\Bll\CommonBll;
 
 class CommonController extends WebController
@@ -16,12 +19,24 @@ class CommonController extends WebController
     //上传图片文件
     public function upload(CommonBll $commonBll)
     {
-        return $commonBll->upload();
+        if (!request()->hasFile('image')) {
+            throw new HifoneException('没有上传图片');
+        }
+        $upload = dispatch(new UploadImageCommand(request()->file('image')));
+        unset($upload['localFile']);
+
+        return $upload;
     }
 
     //上传图片Base64编码
     public function uploadBase64(CommonBll $commonBll)
     {
-        return $commonBll->uploadBase64();
+        if (!request()->hasFile('image')) {
+            throw new HifoneException('没有上传图片');
+        }
+        $upload = dispatch(new UploadBase64ImageCommand(request()->file('image')));
+        unset($upload['localFile']);
+
+        return $upload;
     }
 }
