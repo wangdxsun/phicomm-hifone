ba67a50adb17293394abca053e1166f11e189839
qiuling.jiang <qiuling.jiang@phicomm.com>
2018-02-02 09:38:58 +0800

    继续优化转换链接算法，同时发帖不再使用markdown解析器

--- app/Handlers/Commands/Thread/AddThreadCommandHandler.php
+++ app/Handlers/Commands/Thread/AddThreadCommandHandler.php
@@ -29,7 +29,7 @@ class AddThreadCommandHandler
     public function handle(AddThreadCommand $command)
     {
         $thumbnails = getFirstImageUrl($command->body.$command->images);
-        $body = app('parser.markdown')->convertMarkdownToHtml($command->body);
+        $body = app('parser.link')->parse($command->body);
         $body .= $command->images;
         $data = [
             'user_id'       => $command->user_id,
