933d073e7a047faabb7d83c3af08a8ccbe1282bb
daimeng <meng.dai163@gmail.com>
2018-03-16 08:43:43 +0800

    修改主板块编辑新增页面

--- resources/views/dashboard/nodes/create_edit.blade.php
+++ resources/views/dashboard/nodes/create_edit.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.dashboard')
 
 @section('content')
-@if(isset($sub_menu))
-@include('dashboard.partials.sub-sidebar')
-@endif
-<div class="content-wrapper" id="app">
-    <div class="header sub-header" id="nodes">
+    @if(isset($sub_menu))
+        @include('dashboard.partials.sub-sidebar')
+    @endif
+    <div class="content-wrapper" id="app">
+        <div class="header sub-header" id="nodes">
         <span class="uppercase">
             {{ trans(isset($node) ? 'dashboard.nodes.edit.title' : 'dashboard.nodes.add.title') }}
         </span>
-    </div>
-    <div class="row">
-        <div class="col-sm-12">
-            @if(isset($node))
-            {!! Form::model($node, ['route' => ['dashboard.node.update', $node->id], 'method' => 'patch', 'class' => 'create_form']) !!}
-            @else
-            {!! Form::open(['route' => 'dashboard.node.store', 'method' => 'post', 'class' => 'create_form']) !!}
-            @endif
-            @include('partials.errors')
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                @if(isset($node))
+                    {!! Form::model($node, ['route' => ['dashboard.node.update', $node->id], 'method' => 'patch', 'class' => 'create_form']) !!}
+                @else
+                    {!! Form::open(['route' => 'dashboard.node.store', 'method' => 'post', 'class' => 'create_form']) !!}
+                @endif
+                @include('partials.errors')
                 <fieldset>
                     {{--安卓端版块图片--}}
                     <div class="form-group row">
@@ -50,15 +50,15 @@
                         <div class="col-xs-4">
                             <label>{{ '安卓端版块详情页图片' }}</label><br>
                             <el-upload
-                                class="avatar-uploader"
-                                action="/upload_image"
-                                :show-file-list="false"
-                                :on-success="imageDetailUrlAndroidHandle">
-                            <img v-if="imageDetailUrlAndroid" :src="imageDetailUrlAndroid" class="el-avatar">
-                            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-                        </el-upload>
-                        <el-input  v-model="imageDetailUrlAndroid" placeholder="请输入内容" type="hidden" name="node[android_icon_detail]"></el-input>
-                    </div>
+                                    class="avatar-uploader"
+                                    action="/upload_image"
+                                    :show-file-list="false"
+                                    :on-success="imageDetailUrlAndroidHandle">
+                                <img v-if="imageDetailUrlAndroid" :src="imageDetailUrlAndroid" class="el-avatar">
+                                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                            </el-upload>
+                            <el-input  v-model="imageDetailUrlAndroid" placeholder="请输入内容" type="hidden" name="node[android_icon_detail]"></el-input>
+                        </div>
                     </div>
                     {{--IOS端版块图片--}}
                     <div class="form-group row">
@@ -200,10 +200,10 @@
                         <label>添加版主:</label>
                         <el-select v-model="nodeModerators" multiple placeholder="添加版主">
                             <el-option
-                                v-for="moderator in moderators"
-                                :key="moderator.id"
-                                :label="moderator.username"
-                                :value="moderator.id">
+                                    v-for="moderator in moderators"
+                                    :key="moderator.id"
+                                    :label="moderator.username"
+                                    :value="moderator.id">
                             </el-option>
                         </el-select>
                         <input  type="hidden" class="form-control" :value="nodeModerators" name="nodeModerators">
@@ -235,20 +235,24 @@
                             </el-switch>
                         </el-tooltip>
                         <el-input  v-model="valueShow" placeholder="请输入内容" type="hidden" name="node[is_show]"></el-input>
-                    </div>git 
-                {{--版块描述--}}
-                <div class="form-group">
-                    <label>{{ trans('dashboard.nodes.description') }}</label>
-                    {!! Form::textarea('node[description]', isset($node) ? $node->description : null , ['class' => 'form-control', 'required', 'rows' => 5]) !!}
-                </div>
-                添加版主
-                <div class="form-group">
-                    <label >{{ trans('dashboard.nodes.moderator.add') }}</label>
-                    <input type="text" name="user[name]" class="form-control"
-                    @if (isset($user['name']))
-                    value="{{ $user['name'] }}"
-                    @endif >
-                </div>
+                    </div>
+
+                    {{--<div class="form-group">--}}
+                    {{--<label>{{ '主版块是否支持反馈与建议' }}</label>--}}
+                    {{--<el-tooltip  placement="hidden" >--}}
+                    {{--<el-switch--}}
+                    {{--v-model="valueFeedback"--}}
+                    {{--on-color="#13ce66"--}}
+                    {{--off-color="#ff4949"--}}
+                    {{--on-value=1--}}
+                    {{--off-value=0>--}}
+                    {{--</el-switch>--}}
+                    {{--</el-tooltip>--}}
+                    {{--<el-input name="node[feedback_thread_id]" placeholder="反馈与建议专用贴Id" value="{{ isset($node) ? $node->feedback_thread_id : null }}" ></el-input>--}}
+                    {{--<el-input  v-model="valueFeedback" placeholder="请输入内容" type="hidden" name="node[is_feedback]"></el-input>--}}
+                    {{--</div>--}}
+
+
                     <div class="form-group">
                         <label>{{ trans('dashboard.nodes.prompt.node') }}</label>
                         <el-tooltip  placement="hidden" >
@@ -275,111 +279,111 @@
                         </div>
                     </div>
                 </div>
-            {!! Form::close() !!}
+                {!! Form::close() !!}
+            </div>
         </div>
     </div>
-</div>
-<script>
-    new Vue({
-        el: '#app',
-        data: function () {
-            return {
+    <script>
+        new Vue({
+            el: '#app',
+            data: function () {
+                return {
+                    //安卓端版块图片
+                    imageUrlAndroid: "{{ isset($node) ? ($node->android_icon) : (Input::old('node')['android_icon']) }}",
+                    imageListUrlAndroid: "{{ isset($node) ? ($node->android_icon_list) : (Input::old('node')['android_icon_list']) }}",
+                    imageDetailUrlAndroid: "{{ isset($node) ? ($node->android_icon_detail) : (Input::old('node')['android_icon_detail']) }}",
+                    //IOS端版块图片
+                    imageUrlIos: "{{ isset($node) ? ($node->ios_icon) : (Input::old('node')['ios_icon']) }}",
+                    imageListUrlIos: "{{ isset($node) ? ($node->ios_icon_list) : (Input::old('node')['ios_icon_list']) }}",
+                    imageDetailUrlIos: "{{ isset($node) ? ($node->ios_icon_detail) : (Input::old('node')['ios_icon_detail']) }}",
+                    //H5端版块图片
+                    imageUrl: "{{ isset($node) ? ($node->h5_icon) : (Input::old('node')['icon']) }}",
+                    imageListUrl: "{{ isset($node) ? ($node->h5_icon_list) : (Input::old('node')['icon_list']) }}",
+                    imageDetailUrl: "{{ isset($node) ? ($node->h5_icon_detail) : (Input::old('node')['icon_detail']) }}",
+                    //Web端版块图片
+                    imageDetailUrlWeb: "{{ isset($node) ? ($node->web_icon_detail) : (Input::old('node')['web_icon_detail']) }}",
+                    imageListUrlWeb: "{{ isset($node) ? ($node->web_icon_list) : (Input::old('node')['web_icon_list']) }}",
+                    valuePrompt: "{{  $node->is_prompt or 0  }}",
+                    valueShow: "{{  $node->is_show or 1  }}",
+                    valueFeedback: "{{  $node->is_feedback or 0  }}",
+                    nodeModerators: {!! $nodeModerators or json_encode([])  !!} ,
+                    nodePraModerators: {!! $nodePraModerators or json_encode([])  !!} ,
+                    praModerators: {!! $praModerators !!},
+                    moderators: {!! $moderators !!},
+                };
+            },
+            methods: {
                 //安卓端版块图片
-                imageUrlAndroid: "{{ isset($node) ? ($node->android_icon) : (Input::old('node')['android_icon']) }}",
-                imageListUrlAndroid: "{{ isset($node) ? ($node->android_icon_list) : (Input::old('node')['android_icon_list']) }}",
-                imageDetailUrlAndroid: "{{ isset($node) ? ($node->android_icon_detail) : (Input::old('node')['android_icon_detail']) }}",
+                imageUrlAndroidHandle: function (res) {
+                    this.imageUrlAndroid = res.filename;
+                },
+                imageListUrlAndroidHandle: function (res) {
+                    this.imageListUrlAndroid = res.filename;
+                },
+                imageDetailUrlAndroidHandle: function (res) {
+                    this.imageDetailUrlAndroid = res.filename;
+                },
+
                 //IOS端版块图片
-                imageUrlIos: "{{ isset($node) ? ($node->ios_icon) : (Input::old('node')['ios_icon']) }}",
-                imageListUrlIos: "{{ isset($node) ? ($node->ios_icon_list) : (Input::old('node')['ios_icon_list']) }}",
-                imageDetailUrlIos: "{{ isset($node) ? ($node->ios_icon_detail) : (Input::old('node')['ios_icon_detail']) }}",
+                imageUrlIosHandle: function (res) {
+                    this.imageUrlIos = res.filename;
+                },
+                imageListUrlIosHandle: function (res) {
+                    this.imageListUrlIos = res.filename;
+                },
+                imageDetailUrlIosHandle: function (res) {
+                    this.imageDetailUrlIos = res.filename;
+                },
+
                 //H5端版块图片
-                imageUrl: "{{ isset($node) ? ($node->h5_icon) : (Input::old('node')['icon']) }}",
-                imageListUrl: "{{ isset($node) ? ($node->h5_icon_list) : (Input::old('node')['icon_list']) }}",
-                imageDetailUrl: "{{ isset($node) ? ($node->h5_icon_detail) : (Input::old('node')['icon_detail']) }}",
+                imageUrlHandle: function (res) {
+                    this.imageUrl = res.filename;
+                },
+                imageListUrlHandle: function (res) {
+                    this.imageListUrl = res.filename;
+                },
+                imageDetailUrlHandle: function (res) {
+                    this.imageDetailUrl = res.filename;
+                },
+
                 //Web端版块图片
-                imageDetailUrlWeb: "{{ isset($node) ? ($node->web_icon_detail) : (Input::old('node')['web_icon_detail']) }}",
-                imageListUrlWeb: "{{ isset($node) ? ($node->web_icon_list) : (Input::old('node')['web_icon_list']) }}",
-                valuePrompt: "{{  $node->is_prompt or 0  }}",
-                valueShow: "{{  $node->is_show or 1  }}",
-                valueFeedback: "{{  $node->is_feedback or 0  }}",
-                nodeModerators: {!! $nodeModerators or json_encode([])  !!} ,
-                nodePraModerators: {!! $nodePraModerators or json_encode([])  !!} ,
-                praModerators: {!! $praModerators !!},
-                moderators: {!! $moderators !!},
-            };
-        },
-        methods: {
-            //安卓端版块图片
-            imageUrlAndroidHandle: function (res) {
-                this.imageUrlAndroid = res.filename;
-            },
-            imageListUrlAndroidHandle: function (res) {
-                this.imageListUrlAndroid = res.filename;
-            },
-            imageDetailUrlAndroidHandle: function (res) {
-                this.imageDetailUrlAndroid = res.filename;
-            },
+                imageDetailUrlWebHandle: function (res) {
+                    this.imageDetailUrlWeb = res.filename;
+                },
+                imageListUrlWebHandle: function (res) {
+                    this.imageListUrlWeb = res.filename;
+                },
 
-            //IOS端版块图片
-            imageUrlIosHandle: function (res) {
-                this.imageUrlIos = res.filename;
-            },
-            imageListUrlIosHandle: function (res) {
-                this.imageListUrlIos = res.filename;
-            },
-            imageDetailUrlIosHandle: function (res) {
-                this.imageDetailUrlIos = res.filename;
-            },
-
-            //H5端版块图片
-            imageUrlHandle: function (res) {
-                this.imageUrl = res.filename;
-            },
-            imageListUrlHandle: function (res) {
-                this.imageListUrl = res.filename;
-            },
-            imageDetailUrlHandle: function (res) {
-                this.imageDetailUrl = res.filename;
-            },
-
-            //Web端版块图片
-            imageDetailUrlWebHandle: function (res) {
-                this.imageDetailUrlWeb = res.filename;
-            },
-            imageListUrlWebHandle: function (res) {
-                this.imageListUrlWeb = res.filename;
-            },
-
+            }
+        })
+    </script>
+    <style>
+        .avatar-uploader .el-upload {
+            border: 1px dashed #d9d9d9;
+            border-radius: 6px;
+            cursor: pointer;
+            position: relative;
+            overflow: hidden;
         }
-    })
-</script>
-<style>
-    .avatar-uploader .el-upload {
-        border: 1px dashed #d9d9d9;
-        border-radius: 6px;
-        cursor: pointer;
-        position: relative;
-        overflow: hidden;
-    }
-    .avatar-uploader .el-upload:hover {
-        border-color: #20a0ff;
-    }
-    .avatar-uploader-icon {
-        font-size: 28px;
-        color: #8c939d;
-        width: 178px;
-        height: 178px;
-        line-height: 178px;
-        text-align: center;
-    }
-    .el-avatar {
-        width: 178px;
-        height: 178px;
-        display: block;
-    }
-    .el-upload__input {
-        display: none!important;
-    }
-</style>
+        .avatar-uploader .el-upload:hover {
+            border-color: #20a0ff;
+        }
+        .avatar-uploader-icon {
+            font-size: 28px;
+            color: #8c939d;
+            width: 178px;
+            height: 178px;
+            line-height: 178px;
+            text-align: center;
+        }
+        .el-avatar {
+            width: 178px;
+            height: 178px;
+            display: block;
+        }
+        .el-upload__input {
+            display: none!important;
+        }
+    </style>
 
 @stop
