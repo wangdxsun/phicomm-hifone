ec29538d0ab442b4dc74121ee62d3b04887343e7
qiuling.jiang <qiuling.jiang@phicomm.com>
2018-03-19 10:32:10 +0800

    重构发表回复代码“

--- app/Http/Controllers/App/V1/ReplyController.php
+++ app/Http/Controllers/App/V1/ReplyController.php
@@ -12,13 +12,27 @@ use Hifone\Http\Bll\ReplyBll;
 use Hifone\Http\Controllers\App\AppController;
 use Hifone\Models\Reply;
 use Hifone\Services\Filter\WordsFilter;
+use Auth;
 
 class ReplyController extends AppController
 {
     public function store(ReplyBll $replyBll, WordsFilter $wordsFilter)
     {
-        $reply = $replyBll->createReplyApp();
-        $result = $replyBll->auditReply($reply, $wordsFilter);
+        $reply = $replyBll->createReply();
+        $reply = $replyBll->auditReply($reply, $wordsFilter);
+        $reply->load(['user', 'reply.user']);
+        $reply['liked'] = Auth::check() ? Auth::user()->hasLikeReply($reply) : false;
+        $reply['reported'] = Auth::check() ? Auth::user()->hasReportReply($reply) : false;
+        if ($reply->status == Reply::VISIBLE) {
+            $msg = $reply->reply_id ? '回复成功' : '评论成功';
+        } else {
+            $msg = $reply->reply_id ? '回复已提交，待审核' : '评论已提交，待审核';
+        }
+        $result = [
+            'msg' => $msg,
+            'reply' => $reply,
+        ];
+
         return $result;
     }
 
