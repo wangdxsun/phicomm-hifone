d4585e20acbfaaceeae16df42b28ccc144514712
qiuling.jiang <qiuling.jiang@phicomm.com>
2018-03-26 14:01:58 +0800

    通知中@暂不显示thread_mention

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -72,7 +72,7 @@ class Notification extends BaseModel implements HasPresenter
     //thread_follow 关注帖子尚未考虑
     public function scopeAt($query)
     {
-        return $query->whereIn('type', ['reply_mention', 'reply_reply', 'thread_mention']);
+        return $query->whereIn('type', ['reply_mention', 'reply_reply']);
     }
 
     public function scopeWatch($query)
