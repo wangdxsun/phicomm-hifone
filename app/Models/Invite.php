217081dbf583f5526a442e3def44ce122ce0fba4
qiuling.jiang <qiuling.jiang@phicomm.com>
2018-05-03 20:42:58 +0800

    问题详情页增加是否关注是否举报接口

---
+++ app/Models/Invite.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiuling.jiang
+ * Date: 2018/5/3
+ * Time: 20:24
+ */
+
+namespace Hifone\Models;
+
+class Invite
+{
+    public function scopeQuestion()
+    {
+
+    }
+}
