dca249fc4706ed51ffbaead54bf9ac1df4473754
qiuling.jiang <qiuling.jiang@phicomm.com>
2018-05-11 11:06:39 +0800

    增加导入问题和回答

--- app/Console/Commands/SearchImport.php
+++ app/Console/Commands/SearchImport.php
@@ -9,6 +9,8 @@
 namespace Hifone\Console\Commands;
 
 use Carbon\Carbon;
+use Hifone\Models\Answer;
+use Hifone\Models\Question;
 use Hifone\Models\Thread;
 use Hifone\Models\User;
 use Illuminate\Console\Command;
@@ -42,70 +44,18 @@ class SearchImport extends Command
 
     /**
      * Execute the console command.
-     *
-     * @return mixed
      */
     public function handle()
     {
-        $type = $this->argument('type');
-        if ($type == 'users') {
+        $type = ucfirst($this->argument('type'));
+        if ($type) {
+            $methodName = 'import'.$type;
             $id = $this->option('id');
-            if ($id) {
-                $user = User::find($id);
-                if (!$user) {
-                    $this->error("User $id does't exist");
-                }
-                try {
-                    $user->removeFromIndex();
-                    $user->addToIndex();
-                    $this->info("Import user $id into ElasticSearch Successfully");
-                } catch (\Exception $exception) {
-                    $this->error($exception->getMessage());
-                }
-                return;
+            if (method_exists($this, $methodName)) {
+                $this->$methodName($id);
+            } else {
+                $this->error("method $methodName doesn't exist");
             }
-            User::chunk(1000, function ($users) {
-                foreach ($users as $user) {
-                    try {
-                        $user->removeFromIndex();
-                    } catch (\Exception $exception) {
-                        $this->error($exception->getMessage());
-                    }
-                }
-                $users->addToIndex();
-            });
-
-            $this->info("\r\nImport Users into ElasticSearch Successfully");
-        } elseif ($type == 'threads') {
-            $id = $this->option('id');
-            if ($id) {
-                $thread = Thread::find($id);
-                if (!$thread) {
-                    $this->error("Thread $id does't exist");
-                }
-                try {
-                    $thread->removeFromIndex();
-                    $thread->body = strip_tags($thread->body);
-                    $thread->addToIndex();
-                    $this->info("Import thread $id into ElasticSearch Successfully");
-                } catch (\Exception $exception) {
-                    $this->error($exception->getMessage());
-                }
-                return;
-            }
-            Thread::visible()->chunk(1000, function ($threads) {
-                foreach ($threads as $thread) {
-                    try {
-                        $thread->body = strip_tags($thread->body);
-                        $thread->removeFromIndex();
-                    } catch (\Exception $exception) {
-                        $this->error($exception->getMessage());
-                    }
-                }
-                $threads->addToIndex();
-            });
-
-            $this->info("\r\nImport Threads into ElasticSearch Successfully");
         } else {
             $start = Carbon::now();
             $this->line('Delete Indices...');
@@ -133,6 +83,127 @@ class SearchImport extends Command
             $end = Carbon::now();
             $this->line('共计用时：'.$end->diffInSeconds($start).'秒');
         }
-        return;
+    }
+
+    private function importUser($id = null)
+    {
+        if ($id) {
+            $user = User::find($id);
+            if (!$user) {
+                $this->error("User $id does't exist");
+            }
+            try {
+                $user->removeFromIndex();
+                $user->addToIndex();
+                $this->info("Import user $id into ElasticSearch Successfully");
+            } catch (\Exception $exception) {
+                $this->error($exception->getMessage());
+            }
+        }
+        User::chunk(1000, function ($users) {
+            foreach ($users as $user) {
+                try {
+                    $user->removeFromIndex();
+                } catch (\Exception $exception) {
+                    $this->error($exception->getMessage());
+                }
+            }
+            $users->addToIndex();
+        });
+
+        $this->info("\r\nImport Users into ElasticSearch Successfully");
+    }
+
+    private function importThread($id = null)
+    {
+        if ($id) {
+            $thread = Thread::find($id);
+            if (!$thread) {
+                $this->error("Thread $id does't exist");
+            }
+            try {
+                $thread->removeFromIndex();
+                $thread->body = strip_tags($thread->body);
+                $thread->addToIndex();
+                $this->info("Import thread $id into ElasticSearch Successfully");
+            } catch (\Exception $exception) {
+                $this->error($exception->getMessage());
+            }
+        }
+        Thread::visible()->chunk(1000, function ($threads) {
+            foreach ($threads as $thread) {
+                try {
+                    $thread->body = strip_tags($thread->body);
+                    $thread->removeFromIndex();
+                } catch (\Exception $exception) {
+                    $this->error($exception->getMessage());
+                }
+            }
+            $threads->addToIndex();
+        });
+
+        $this->info("\r\nImport Threads into ElasticSearch Successfully");
+    }
+
+    private function importQuestion($id = null)
+    {
+        if ($id) {
+            $question = Question::find($id);
+            if (!$question) {
+                $this->error("Question $id does't exist");
+            }
+            try {
+                $question->removeFromIndex();
+                $question->body = strip_tags($question->body);
+                $question->addToIndex();
+                $this->info("Import question $id into ElasticSearch Successfully");
+            } catch (\Exception $exception) {
+                $this->error($exception->getMessage());
+            }
+        }
+        Question::visible()->chunk(1000, function ($questions) {
+            foreach ($questions as $question) {
+                try {
+                    $question->body = strip_tags($question->body);
+                    $question->removeFromIndex();
+                } catch (\Exception $exception) {
+                    $this->error($exception->getMessage());
+                }
+            }
+            $questions->addToIndex();
+        });
+
+        $this->info("\r\nImport Questions into ElasticSearch Successfully");
+    }
+
+    private function importAnswer($id = null)
+    {
+        if ($id) {
+            $question = Answer::find($id);
+            if (!$question) {
+                $this->error("Answer $id does't exist");
+            }
+            try {
+                $question->removeFromIndex();
+                $question->body = strip_tags($question->body);
+                $question->addToIndex();
+                $this->info("Import answer $id into ElasticSearch Successfully");
+            } catch (\Exception $exception) {
+                $this->error($exception->getMessage());
+            }
+        }
+        Answer::with('question')->chunk(100, function ($answers) {
+            foreach ($answers as $answer) {
+                try {
+                    $answer->body = strip_tags($answer->body);
+                    $answer->removeFromIndex();
+                } catch (\Exception $exception) {
+                    $this->error($exception->getMessage());
+                }
+            }
+            $answers->addToIndex();
+        });
+
+        $this->info("\r\nImport Questions into ElasticSearch Successfully");
     }
 }
