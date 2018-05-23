10b05f6522576c751c9c988186d30fcf5827c221
daoxin.wang <daoxin.wang@phicomm.com>
2018-05-23 11:20:07 +0800

    app当前用户信息加入smart_score智慧果

--- app/Http/Controllers/App/V1/UserController.php
+++ app/Http/Controllers/App/V1/UserController.php
@@ -22,6 +22,7 @@ use Hifone\Models\Section;
 use Hifone\Models\User;
 use Hifone\Services\Filter\WordsFilter;
 use Auth;
+use Hifone\Services\Guzzle\Score;
 
 class UserController extends AppController
 {
@@ -47,6 +48,7 @@ class UserController extends AppController
             }
         }
         $user['isAdmin'] = ($user->role =='管理员' || $user->role =='创始人');
+        $user['smart_score'] = app(Score::class)->getScore($user->phicomm_id);
 
         return $user;
     }
