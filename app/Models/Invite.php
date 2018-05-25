a2334880ce73e3a4f125ef2584c0bfda80633834
daoxin.wang <daoxin.wang@phicomm.com>
2018-05-25 10:55:01 +0800

    编写Invite model并重建Invites表唯一索引

--- app/Models/Invite.php
+++ app/Models/Invite.php
@@ -8,10 +8,28 @@
 
 namespace Hifone\Models;
 
-class Invite
+class Invite extends BaseModel
 {
-    public function scopeQuestion()
-    {
+    protected $guarded = ['id'];
 
+    public $rules = [
+        'from_user_id'        => 'required|int',
+        'to_user_id'    => 'required|int',
+        'question_id'  => 'required|int',
+    ];
+
+    public function from()
+    {
+        return $this->belongsTo(User::class, 'from_user_id');
+    }
+
+    public function to()
+    {
+        return $this->belongsTo(User::class, 'to_user_id');
+    }
+
+    public function scopeQuestion($query, Question $question)
+    {
+        return $query->where('question_id', $question->id);
     }
 }
