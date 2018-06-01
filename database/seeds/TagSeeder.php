e19f1568f07acd792bad4934364d011479406404
daimeng <meng.dai163@gmail.com>
2018-06-01 15:45:43 +0800

    插自动标签的脚本

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class TagSeeder extends Seeder
 {
     /**
@@ -11,65 +12,78 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
+        $tag_type_id = \Hifone\Models\TagType::where('display_name', '自动标签')->id;
         DB::table('tags')->insert([
             'name'   => '盒子',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => 'R1',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => 'K2系列',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => 'K3系列',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => '内容贡献量多',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => '回帖质量高',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => '发帖质量高',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => '新用户',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => '近期登陆',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => '2周活跃高',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => '4周活跃高',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
         DB::table('tags')->insert([
             'name'   => '8周活跃高',
             'channel' => 0,
-            'count'  => 0
+            'count'  => 0,
+            'tag_type_id' => $tag_type_id,
         ]);
 
     }
